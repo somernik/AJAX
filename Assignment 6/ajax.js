a41890e50f4e050003da794601c0b85a7de98b87
Sarah Omernik <somernik@madisoncollege.edu>
2017-10-09 17:26:15 -0500

    Finishing assignment 6

--- Assignment 6/ajax.js
+++ Assignment 6/ajax.js
@@ -1,11 +1,11 @@
 function init() {
-    alert("hello");
 
     var xhr = new XMLHttpRequest();
     var url = "hello.php";
 
     xhr.open("GET", url);
 
+	// callback
     xhr.onreadystatechange = function() {
         if (xhr.readyState == 4) {
             alert(xhr.responseText);
